c8641ff6c5ee63f071fcc6abd04190253a0edef5
cesar_zapet <cesarzapet@hotmail.com>
2020-08-26 03:13:46 -0600

    agregado componente login y registro usuario (maquetado)

--- protech-ui/src/app/app.module.ts
+++ protech-ui/src/app/app.module.ts
@@ -20,6 +20,8 @@ import { CatalogoComponent } from './components/catalogo/catalogo.component';
 import { TarjetasProductosComponent } from './components/tarjetas-productos/tarjetas-productos.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { CarritocompraComponent } from './components/carritocompra/carritocompra.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
 
 
 @NgModule({
@@ -37,7 +39,9 @@ import { CarritocompraComponent } from './components/carritocompra/carritocompra
     AppComponent,
     DetalleproductoComponent,
     IngresoproductosComponent,
-    CarritocompraComponent
+    CarritocompraComponent,
+    LoginComponent,
+    RegistrarUsuarioComponent
   ],
   imports: [
     BrowserModule,
