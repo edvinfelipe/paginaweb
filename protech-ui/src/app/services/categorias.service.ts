23216b006762c80c2834b84b1ad6e06510fef767
Pablo <pablo.giron.97@gmail.com>
2020-09-15 03:13:13 -0600

    ADD-FRONT-CATALOGO
    Creación de los servicios de categoria, componente de catalogo terminado, cambios de diseño menores y optimización de código

--- protech-ui/src/app/services/categorias.service.ts
+++ protech-ui/src/app/services/categorias.service.ts
@@ -3,23 +3,18 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root' //Servicio inyectable en cualquier lugar.
+  providedIn: 'root'
 })
-export class MarcasService {
+export class CategoriasService {
 
-  constructor(private http: HttpClient) { 
-    /*console.log("Constructor, servicio de marcas")*/
-  }
-  
-  getMarcas(){
-
+  constructor( private http: HttpClient) { }
+  getCategorias(){
     const headers = new HttpHeaders({
       'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3VhcmlvIjp7Il9pZCI6IjVmNTU0ZDhjODgzNzE1NWYyMTllOTM0YSIsImNvZF9lbXBsZWFkbyI6IkVNXzExIiwidXNlcm5hbWUiOiJhZG1pbiIsIl9fdiI6MH0sImlhdCI6MTU5OTYwMTUxNCwiZXhwIjoxNTk5Nzc0MzE0fQ.BI8FgxCGtpWpM6AE0XrDH-GUhdx5txMfnqfQlkVRf8Y'
     });
 
-    return this.http.get('https://api-protech.herokuapp.com/api/marca/', {headers})
-      .pipe( map( data => data['marcas']));
+    return this.http.get('https://api-protech.herokuapp.com/api/categoria/', {headers})
+      .pipe( map( data => data['categorias']));
 
   }
-
 }
