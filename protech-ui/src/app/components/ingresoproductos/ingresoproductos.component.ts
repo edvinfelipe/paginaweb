26e1c855b2ee5384651dbf62d49c3d3f36545534
Cristian <cristianmmr09@gmail.com>
2020-10-10 20:57:53 -0600

    UPD-FRONT: Se agregaron validaciones a los ingresos de productos

--- protech-ui/src/app/components/ingresoproductos/ingresoproductos.component.ts
+++ protech-ui/src/app/components/ingresoproductos/ingresoproductos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+//componentes de form
 declare var $: any;
 
 
@@ -12,7 +13,9 @@ export class IngresoproductosComponent implements OnInit {
   marcas: string[] = ['Marca 1', 'Marca 2', 'Marca 3'];
   categorias: string[] = ['Categoría 1', 'Categoría 2', 'Categoría 3'];
   urls = new Array<string>();
-  especificacion: string;
+  sEspecificaciones: string = '';
+  especificaciones = new Array<string>();
+  booleano = false;
   constructor() {
   }
 
@@ -24,9 +27,88 @@ export class IngresoproductosComponent implements OnInit {
   }
 
   obtenerEspecificacion(){
-    console.log('Especificación ingresada: ')
+    var re = /[0-9a-zA-ZÀ-ÿ\s\u00f1\u00d1:.;!#$%&/()=?¡¿/*+>_-]$/;
+    var input = re.exec((<HTMLInputElement>document.getElementById("especificacion")).value);
+    var longitud = (<HTMLInputElement>document.getElementById("especificacion")).value;
+    if (input && (this.validarLongitud() + longitud.length) <= 800)
+    {
+      var valor = (<HTMLInputElement>document.getElementById("especificacion")).value;
+      this.especificaciones.push(valor);
+      (<HTMLInputElement>document.getElementById("especificacion")).value = '';
+      this.crearEspecificaciones();
+    }
+    else if (!input)
+    {
+      alert("Ingrese letras, números y SOLO los caracteres especiales especificados: :.;!#$%&/()=?¡¿/*-+>_ (no ingrese comas, importante)")
+    }
+    else if (this.validarLongitud() === 800)
+    {
+      alert("Ya no puede ingresar más especificaciones");
+    }
+    else if ((this.validarLongitud() + longitud.length) > 800)
+    {
+      alert("Ya no puede ingresar más especificaciones (a menos que la siguiente especificación tenga " + (800-this.validarLongitud()) + " letras)");
+    }
+    else
+    {
+      alert("Especificación mal ingresada");
+    }
   }
-  
+
+  validarLongitud()
+  {
+    var longitud = 0;
+    for (let i = 0; i < this.especificaciones.length; i++)
+    {
+      longitud = longitud + this.especificaciones[i].length;
+    }
+    return longitud;
+  }
+
+  crearEspecificaciones()
+  {
+    this.sEspecificaciones = '';
+    for (let i = 0; i < this.especificaciones.length; i++)
+    {
+      if (i === this.especificaciones.length - 1)
+      {
+        this.sEspecificaciones = this.sEspecificaciones + this.especificaciones[i];
+        console.log(this.sEspecificaciones);
+      }
+      else
+      {
+        this.sEspecificaciones = this.sEspecificaciones + this.especificaciones[i] + ', ';
+      }
+    }
+  }
+
+  asignarValor(){
+    this.booleano = (<HTMLInputElement>document.getElementById('cbDescuento')).checked;
+  }
+
+  validarIngreso()
+  {
+    if ((<HTMLInputElement>document.getElementById('cbDescuento')).checked)
+    {
+      if (!(<HTMLInputElement>document.getElementById('inputDescuento')).checked)
+      {
+        console.log('No se puede ingresar');
+      }
+    }
+  }
+
+  verificar()
+  {
+    
+  }
+
+  eliminarEspecificaciones(especificacion)
+  {
+    var i = this.especificaciones.indexOf(especificacion);
+    if (i != -1){
+      this.especificaciones.splice(i, 1);
+    }
+  }
 
   detectFiles(event) {
     this.urls = [];
@@ -40,7 +122,6 @@ export class IngresoproductosComponent implements OnInit {
         reader.readAsDataURL(file);
       }
     }
- 
   }
 
   imgPorDefecto(urls: string[])
