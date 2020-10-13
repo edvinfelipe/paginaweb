f94cd967ae61e9eb061e961eebc9593ccca93822
Edvin Felipe <eddigzol@gmail.com>
2020-10-12 19:33:04 -0600

    deploy

--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -3,23 +3,23 @@ const express = require('express')
 const app = express()
 
 // Endpoints de la API
-// app.use(require('./categoria'))
-// app.use(require('./marca'))
+app.use(require('./categoria'))
+app.use(require('./marca'))
 // app.use(require('./usuario'))
-// app.use(require('./login'))
+app.use(require('./login'))
 app.use(require('./producto'))
-// app.use(require('./clientes_registrados'))
+app.use(require('./clientes_registrados'))
 app.use(require('./factura'))
 app.use(require('./detalle_factura'))
 
-if( process.env.NODE_ENV !== 'dev' ){
-    app.use(require('./uploadcloud'))
-}else{
+// if( process.env.NODE_ENV !== 'dev' ){
+//     app.use(require('./uploadcloud'))
+// }else{
 
     app.use(require('./upload'))
-}
+// }
 
-app.use(require('./imagenes'))
+// app.use(require('./imagenes'))
 
 
 module.exports = app
