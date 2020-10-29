8a251ac92f41d99e0c5bcf73395aea32bae0baa2
cesar_zapet <cesarzapet@hotmail.com>
2020-10-29 02:29:08 -0600

    uso de servicio para ingreso de usuario
    conecccion de componete a servicio para ingreso de nuevo usuario

--- protech-ui/src/app/components/usuarios/usuarios.component.ts
+++ protech-ui/src/app/components/usuarios/usuarios.component.ts
@@ -1,13 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import {  UsuariosService } from '../../services/usuarios.service';
 
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
-  styleUrls: ['./usuarios.component.css']
+  styleUrls: ['./usuarios.component.css'],
+  providers: [UsuariosService]
 })
 export class UsuariosComponent implements OnInit {
+  datos = [{codigo: '',username:'',password:''}];
+  datos1 ={codigo:'',username:'',password:''};
 
-  constructor() { }
+  constructor(private api:UsuariosService) { }
+
+
+
+  Agregar=()=>{
+    if(this.datos1.codigo!=''){   //agregar el resto de campos
+      if(this.datos1.username!=""){
+        if(this.datos1.password!=""){
+          
+     
+    this.api.Agregar(this.datos1).subscribe(
+      data => {
+        alert('ingreso existoso');
+        this.datos.push(data);
+      },
+      error =>{
+        console.log(error);
+      }
+    );
+  }
+  else{
+    alert('faltan password para agregar el nuevo empleado')
+  }
+  }
+  else{
+    alert('faltandescripcion para agregar el nuevo empleado')
+  }
+}
+  else{
+    alert('faltan codigo para agregar el nuevo empleado')
+  }
+  }
 
   ngOnInit(): void {
   }
--- protech-ui/src/app/services/usuarios.service.ts
+++ protech-ui/src/app/services/usuarios.service.ts
@@ -1,6 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
 
 export class  UsuariosService {
+    constructor(private http: HttpClient) {
+        /*console.log("Constructor, servicio de marcas")*/
+      }
+    
+  Agregar(datos):Observable<any> {
+
+    const headers = new HttpHeaders({
+      'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3VhcmlvIjp7Il9pZCI6IjVmNTU0ZDhjODgzNzE1NWYyMTllOTM0YSIsImNvZF9lbXBsZWFkbyI6IkVNXzExIiwidXNlcm5hbWUiOiJhZG1pbiIsIl9fdiI6MH0sImlhdCI6MTU5OTYwMTUxNCwiZXhwIjoxNTk5Nzc0MzE0fQ.BI8FgxCGtpWpM6AE0XrDH-GUhdx5txMfnqfQlkVRf8Y'
+    });
+    const evento={cod_empleado:datos.codigo,username:datos.username,password:datos.password};
+    console.log(evento);
+    return this.http.post('https://api-protech.herokuapp.com/api/usuarios/', {headers})
+
+  }
 
     }
   
