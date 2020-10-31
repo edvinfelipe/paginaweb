6ccaf2bad53da79f89ab828a2f4bdef3a5ec649c
Carlos Menchu <carlosmenchu2@gmail.com>
2020-10-30 23:31:18 -0600

    UPD-FRONT-CORREXION

--- protech-ui/src/app/app.module.ts
+++ protech-ui/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-=======
-import { FormsModule } from '@angular/forms';
+
+
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
