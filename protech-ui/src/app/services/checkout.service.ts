381c9606be9dd3651b190ca8655528606302b419
Jorge Barrientos <elgigototo@hotmail.com>
2020-11-02 02:22:56 -0600

    UPD-FRONT-CHECKOUT
    Se reconfiguro la forma de enviar el post, ahora se envia un arreglo de jsons, especificamente el detalle del envio, la factura, y el detalle de los productos

--- protech-ui/src/app/services/checkout.service.ts
+++ protech-ui/src/app/services/checkout.service.ts
@@ -11,59 +11,15 @@ export class CheckoutService {
   constructor(private http: HttpClient) { 
     console.log("servicio checkout listo");
   }
-  getfechaformato(){
-    let date = new Date();
-    let fecha = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() ;
-    return fecha;
-  }
-  gethoraformato(){
-    let date = new Date();
-    let horas = date.getHours(); let minutos = date.getMinutes(); let segundos = date.getSeconds();
-    
-    let h; let m; let s;
-    if (horas <10) {
-      h = '0' +horas;
-    }else{
-      h = horas;
-    }
-    if (minutos <10) {
-      m = '0' +minutos;
-    }else{
-      m = minutos;
-    }
-    if (segundos <10) {
-      s = '0' +segundos;
-    }else{
-      s = segundos;
-    }
-    let horafactura = h+":"+m+":"+s;
-    return horafactura;
-  }
+
 
   getproducto(id: number){
     String(id);
     return this.http.get(`https://api-protech.herokuapp.com/api/producto/${id}`);
   }
 
-  postfacturaconcliente(total: number, cliente: String ){
-    let fecha = this.getfechaformato();
-    let horafactura = this.gethoraformato();
-
-    let fechayhorafactura = fecha + 'T'+ horafactura;    
-    let body = {total: total, fecha_venta: fechayhorafactura, cliente_factura:cliente}
-
-    return this.http.post('https://api-protech.herokuapp.com/api/factura/', body);
-  }
-
-  postfacturasincliente(total: number ){
-
-    let fecha = this.getfechaformato();
-    let horafactura = this.gethoraformato();
-
-    let fechayhorafactura = fecha + 'T'+ horafactura;    
-    let body = {total: total, fecha_venta: fechayhorafactura}
-
-    return this.http.post('https://api-protech.herokuapp.com/api/factura/', body);
-
+  PostFactura(factura: any){
+    console.log(factura);
+    return this.http.post('https://api-protech.herokuapp.com/api/factura/', factura);
   }
 }
