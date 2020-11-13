778e2870c415af71f69f217eafe464c1242f5dbc
Carlos Menchu <carlosmenchu2@gmail.com>
2020-11-13 03:28:06 -0600

    ADD-FRONT-Crecion de servicio de busqueda

--- protech-ui/src/app/components/shared/navbar/navbar.component.ts
+++ protech-ui/src/app/components/shared/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
+import { debounceTime } from "rxjs/operators";
 
 import { CategoriasService } from "../../../services/categorias.service";
 import { LoginComponent } from "../../login/login.component";
 import { RegistroComponent } from "../../registro/registro.component";
 import { LoginService } from "../../../services/login.service";
+import { FiltrobusquedaService } from "../../../services/filtrobusqueda.service";
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
@@ -16,16 +19,42 @@ export class NavbarComponent implements OnInit {
 
   categorias: any[] = [];
   isLogged:boolean = false;
-  constructor(private _cateogiriasService:CategoriasService,private modalService: NgbModal, private _loginService: LoginService,
+  emailCtrl = new FormControl('',[]);
+  productos:any[] = [];
+  buscando:boolean=false;
+  constructor(private _cateogiriasService:CategoriasService,private _filtroBusqueda:FiltrobusquedaService,private modalService: NgbModal, private _loginService: LoginService,
               private Router: Router) {
+
+    this.getCategorias();
+
+    this.emailCtrl.valueChanges
+    .pipe(debounceTime(350))
+    .subscribe(value=>{
+      if(value!=""){
+        this.buscando=true;
+        this.getProductos(value);
+      }else{
+        this.buscando=false;
+      }
+    })
+  }
+
+  getCategorias(){
+
     this._cateogiriasService.getCategorias()
-      .subscribe( (dataCategorias: any) => {
-        this.categorias = dataCategorias;
-      });
-   }
+    .subscribe( (dataCategorias: any) => {
+      this.categorias = dataCategorias;
+    });
+  }
 
   HistorialLocation() {
     this.Router.navigate(['historialcompras']);
+  }
+
+  getProductos(producto){
+    this._filtroBusqueda.getResultadoBusqueda(producto).subscribe((data:any)=>{
+      this.productos =  data['productos'];
+    });
   }
 
   ngOnInit(): void {
@@ -39,7 +68,6 @@ export class NavbarComponent implements OnInit {
 
   openRegistro(){
     const modalRef = this.modalService.open(RegistroComponent);
-    // this.modalService.open(RegistroComponent, { size: 'lg' });
   }
 
   onCheckUser(){
